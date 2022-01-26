1d25778a335d147f50bfbf0bd1f420044a4e4ca0
badcompwaifu <phasedkamo@gmail.com>
2022-01-26 01:41:16 -0300

    did some css

---
+++ pentaworld.js
@@ -0,0 +1,10 @@
+var word = 'panda';
+var arr = '';
+
+function getKeypress(current) {
+    // var current_key = document.getElementById('keybutton').innerHTML;
+    var c = document.getElementsByClassName(current);
+    console.log(c);
+    // add current key to key position in html
+};
+
